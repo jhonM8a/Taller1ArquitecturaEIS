4ba25f6fabc073f9245d5139f9b223bb9302d86b
Jhon Ochoa <jhon.ochoa@openintl.com>
2024-11-18 19:49:19 -0500

    initial commit

---
+++ App2Interfaces/src/main/java/com/talleruno/app2interfaces/App2Interfaces/documentos/Documento.java
@@ -0,0 +1,10 @@
+package com.talleruno.app2interfaces.App2Interfaces.documentos;
+
+public interface Documento {
+	
+	void setContenido(String contenido);
+
+	void dibujar();
+
+	void imprimir();
+}
